a598459295e9c9c168a255c5a9bf6339cb3d387a
MedanXploit12 <75316440+MRKALIT09@users.noreply.github.com>
2021-02-14 12:06:08 +0700

    Add files via upload

---
+++ ImDark.sh
@@ -0,0 +1,125 @@
+#!bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+echo -n "   [ Masukan Passwrod Tools Nya ] : "
+read pw;
+if [ $pw = 'MANDO-XPLOIT' ]
+then
+echo $me "  [×]$ku Harap Menunggu Ya Bwang "
+sleep 5
+echo $i  "  [✓]$i Password Benar Bwang    "
+echo $i  "  [•] FOLLOW AKUN FB GW YA BWANG"
+xdg-open https://www.facebook.com/profile.php?id=100027700041268
+else
+echo $me "  [×]$ku Harap Menunggu Ya Bwang "
+sleep 5
+echo $me "  [!]$cy Password Salah Bwang Minta Password Ke Author "
+sleep 5
+xdg-open https://www.facebook.com/profile.php?id=100027700041268
+exit
+fi
+echo $cy "                  [ Ini Adalah Script Perusak Hp ] "
+echo $cy "             [ Mohon Di Gunakan Dengan Bijak Ya Bwang ] "
+echo $me "          ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁ ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁ ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁  "
+echo
+echo $i "           [×]$ku CREATED BY : /MANDO-XPLOIT   "
+echo $i "           [×]$ku WhatsApp Me: 0853-6152-4681  "
+echo $i "           [×]$ku GITHUB : https://github.com/MRKALIT09"
+echo $me "          ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁ ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁ ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁  "
+echo
+echo $ku "	           Pilih Virus Nya Bwang Jago    "
+echo $me "        [  [1]  [×]$i  BLACK MAGMA    ] "
+echo $me "        [  [2]  [×]$i  VIRUS TROJANS  ]"
+echo $me "        [  [3]  [×]$i  VIRUS ENERGY   ]"
+echo $me "        [  [4]  [×]$i  VIRUS BLACKHAT ]"
+echo $me "        [  [0]  [×]$i  EXIT PROGRAM   ]"
+echo
+read -p  "            Masukan Menu Nya : " pill
+if [ $pill = "1" ]
+then
+echo -n "            Masukan IP ADDRES Nya : "
+read b
+echo -n "            Masukan HMEI Hp Nya   : "
+read t
+echo -n "            Masukan Jumlah Port Nya :"
+read s
+echo $ku "          [×] Harap Menunggu  "
+sleep 8
+while [ true ]; do
+echo $me " Virus BlackMagma $i Succes Send To $ku $b  Pada Port $s"
+done
+fi
+if [ $pill = "2" ]
+then
+echo -n "             Masukan IP ADDRES Nya :"
+read b
+echo -n "             Masukan HMEI Hp Nya   :"
+read t
+echo -n "             Masukan Jumlah Port Nya :"
+read s
+echo $ku    "        [×] Harap Menunggu  "
+sleep 8
+while [ true ]; do
+echo $me " Virus Trojans $i Succes Send To $b $ku Pada Port $s "
+done
+fi
+if [ $pill = "3" ]
+then
+echo -n "            Masukan IP ADDRES Nya : "
+read b
+echo -n "            Masukan HMEI Hp Nya : "
+read t
+echo -n "            Masukan Jumlah Port : "
+read s
+echo $ku      "         [×] Harap Menunggu  "
+sleep 8
+while [ true ]; do
+echo $me " Virus ENERGY Succes Send $i To IP $b $ku Pada Port $s"
+done
+fi
+if [ $pill = "4" ]
+then
+echo -n "  	 Masukan IP ADDRES Hp Nya : "
+read b
+echo -n "         Masukan HMEI Hp Nya      : "
+read t
+echo -n "         Masukan Jumlah Port Nya  : "
+read s
+echo $ku "        [×] Harap Menunggu "
+sleep 8
+while [ true ]; do
+echo $me " VIRUS BLACKHAT $i SUCCES SEND TO IP $b $me PADA PORT $s"
+done
+fi
+if [ $pill = "0" ]
+then
+echo $me " [×] $i JIKA SCRIP RUSAK ANDA AKAN OTOMATIS DI ARAHKAN KE FB / WA SAYA"
+sleep 3
+xdg-open https://www.facebook.com/profile.php?id=100027700041268
+fi
+echo $me " [✓] $i KAMU BARU SAJA DI ARAHKAN KE FB SAYA "
+sleep 4
+echo $me " [×] $i SEKARANG KE WhatApp Saya "
+xdg-open https://wa.me/6285361524681
+echo $me " [✓] $i KAMU BARU SAJA DI ARAHKAN KE WA SAYA "
+sleep 4
+echo $me " [!] $i HUBUNGI SAYA JIKA HANYA SCRIP RUSAK YA TOD "
+sleep 4
+echo $me " [•] $ku PROSES LOG OUT ..."
+sleep 10
+echo $me " [✓] $i LOG OUT SUCCES ..."
+exit
+fi
+
+
+
+
+
+
